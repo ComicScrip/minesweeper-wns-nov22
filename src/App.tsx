72e0be51ce604f63d668129fc44ef0adbdc06cd4
Pierre Genthon <genthon.pierre@gmail.com>
2023-04-17 21:42:15 +0200

    game

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import "./App.css";
+import { Board, Cell, createBoard, forEachCell, getGameStatus } from "./Board";
+import produce from "immer";
+
+const BOARD_SIZE = 5;
+
+function App() {
+  const [board, setBoard] = useState(createBoard(BOARD_SIZE));
+  const [boardIsRevealed, setBoardIsRevealed] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+
+  const reload = () => {
+    setBoard(createBoard(BOARD_SIZE));
+    setIsFinished(false);
+  };
+
+  const handleCellClick = (
+    cell: Cell,
+    board: Board,
+    setBoard: any,
+    setIsFinished: any
+  ) => {
+    const newBoard = produce(board, (draft: any) => {
+      if (cell.val === "bomb") {
+        draft[cell.x][cell.y].backgroundColor = "red";
+        forEachCell(draft, (cell) => {
+          cell.revealed = true;
+        });
+      } else {
+        draft[cell.x][cell.y].backgroundColor = "green";
+      }
+      draft[cell.x][cell.y].revealed = true;
+    });
+
+    setBoard(newBoard);
+
+    const status = getGameStatus(newBoard);
+    if (status === "lost") {
+      setIsFinished(true);
+
+      setTimeout(() => alert("YOU LOOSE :("), 200);
+    } else if (status === "won") {
+      setTimeout(() => alert("CONGRATS :D"), 200);
+      setIsFinished(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <button
+          disabled={isFinished}
+          onClick={() => setBoardIsRevealed(!boardIsRevealed)}
+        >
+          {boardIsRevealed ? "Stop cheating" : "Cheat"}
+        </button>
+        <button
+          style={{ backgroundColor: isFinished ? "orange" : "" }}
+          onClick={reload}
+        >
+          Reload
+        </button>
+        <table style={{ opacity: isFinished ? 0.7 : 1 }}>
+          <tbody>
+            {board.map((row, rowIndex) => {
+              return (
+                <tr key={rowIndex}>
+                  {row.map((cell) => (
+                    <td
+                      style={{
+                        backgroundColor: cell.backgroundColor,
+                      }}
+                      key={cell.y}
+                      onClick={() =>
+                        handleCellClick(cell, board, setBoard, setIsFinished)
+                      }
+                    >
+                      {(cell.revealed || boardIsRevealed) &&
+                        (cell.val === "bomb" ? "💣" : cell.val)}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board.ts
@@ -0,0 +1,103 @@
+export interface Cell {
+  x: number;
+  y: number;
+  val: number | "bomb";
+  revealed: boolean;
+  backgroundColor: string;
+}
+
+export type Board = Cell[][];
+
+export const createEmptyBoard = (size: number): Board => {
+  const cells: Board = new Array(size);
+  for (let x = 0; x < size; x++) {
+    cells[x] = new Array(size);
+    for (let y = 0; y < size; y++) {
+      cells[x][y] = {
+        x,
+        y,
+        revealed: false,
+        val: 0,
+        backgroundColor: "transparent",
+      };
+    }
+  }
+  return cells;
+};
+
+export const forEachCell = (board: Board, fn: (cell: Cell) => void) => {
+  board.flat().forEach((cell) => {
+    fn(cell);
+  });
+};
+
+export const populateWithBombs = (board: Board, bombRatio = 0.2) => {
+  const allCells = board.flat();
+  const indexes = allCells.map((_, i) => i);
+  const nbBombs = Math.round(allCells.length * bombRatio);
+  for (let i = 0; i < nbBombs; i += 1) {
+    const randomIdx = Math.floor(Math.random() * indexes.length);
+    const cellIdx = indexes[randomIdx];
+    const toUpdate = allCells[cellIdx];
+    console.log({ allCells, indexes, randomIdx, cellIdx, toUpdate });
+
+    if (toUpdate) toUpdate.val = "bomb";
+    // removing this index since we already placed a bomb on this cell
+    indexes.splice(randomIdx, 1);
+  }
+};
+
+export const getNeighbors = (board: Board, cell: Cell): Cell[] => {
+  const neighbors = [];
+  if (cell.y !== 0) {
+    neighbors.push(board[cell.x][cell.y - 1]);
+  }
+  if (cell.y !== 0 && cell.x !== board.length - 1) {
+    neighbors.push(board[cell.x + 1][cell.y - 1]);
+  }
+  if (cell.x !== board.length - 1) {
+    neighbors.push(board[cell.x + 1][cell.y]);
+  }
+  if (cell.x !== board.length - 1 && cell.y !== board.length - 1) {
+    neighbors.push(board[cell.x + 1][cell.y + 1]);
+  }
+  if (cell.y !== board.length - 1) {
+    neighbors.push(board[cell.x][cell.y + 1]);
+  }
+  if (cell.y !== board.length - 1 && cell.x !== 0) {
+    neighbors.push(board[cell.x - 1][cell.y + 1]);
+  }
+  if (cell.x !== 0) {
+    neighbors.push(board[cell.x - 1][cell.y]);
+  }
+  if (cell.x !== 0 && cell.y !== 0) {
+    neighbors.push(board[cell.x - 1][cell.y - 1]);
+  }
+  return neighbors;
+};
+
+export const populateWithNeighborsCount = (board: Board) => {
+  forEachCell(board, (cell) => {
+    if (cell.val !== "bomb") {
+      cell.val = getNeighbors(board, cell).filter(
+        (cell) => cell.val === "bomb"
+      ).length;
+    }
+  });
+};
+
+export const createBoard = (size = 5, bombRatio = 0.2) => {
+  const b = createEmptyBoard(size);
+  populateWithBombs(b, bombRatio);
+  populateWithNeighborsCount(b);
+  return b;
+};
+
+export const getGameStatus = (board: Board): "won" | "lost" | "inProgress" => {
+  const allCells = board.flat();
+  const notBombs = allCells.filter((cell) => cell.val !== "bomb");
+  const lost = allCells.some((cell) => cell.revealed && cell.val === "bomb");
+  if (lost) return "lost";
+  const won = notBombs.every((cell) => cell.revealed);
+  return won ? "won" : "inProgress";
+};
